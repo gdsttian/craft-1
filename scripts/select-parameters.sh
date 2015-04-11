dce6a061bec0cc89896fbf8a177e9635acae5555
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-04-11 09:32:39 +0300

    Run parameter selection using cross-validation

---
+++ scripts/select-parameters.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run parameter selection for separated standoff data.
+
+set -e
+set -u
+
+# http://stackoverflow.com/a/246128
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+CRAFTROOT="$SCRIPTDIR/.."
+
+INPUTDIR="$CRAFTROOT/train-and-devel-standoff-separated"
+
+OUTPUTDIR="$CRAFTROOT/parameter-selection-results"
+
+RANDOMSEED="0c31e182-0d8a-40d8-94da-b2e1aa41ec8f"
+
+EVALUATOR="$HOME/git_checkout/semsuite/semsuite eval -a -x 10 -r $RANDOMSEED"
+
+rm -rf "$OUTPUTDIR"
+mkdir "$OUTPUTDIR"
+
+for d in "$INPUTDIR"/*; do
+    t=`basename $d`
+    o="$OUTPUTDIR/$t"
+    nohup $EVALUATOR \
+	-c2 '(2**{5..-10..-1})' \
+	-b 'B-*:{-5..5..0.5}' \
+	$d > $o 2>nohup.out.$t&
+done
